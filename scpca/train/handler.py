2af0f5a0a19784824d5d878285fc4108c11f93c9
Harald Vohringer <harald.voeh@gmail.com>
2023-09-10 19:10:19 +0200

    added docstring to handler SVIBaseHandler

--- scpca/train/handler.py
+++ scpca/train/handler.py
@@ -11,19 +11,46 @@ from tqdm import tqdm  # type: ignore
 
 class SVIBaseHandler:
     """
-    Helper object that abstracts some of numpyros complexities. Inspired
+    Helper object that abstracts some of pyros complexities. Inspired
     by an implementation of Florian Wilhelm.
-    :param model: A numpyro model.
-    :param guide: A numpyro guide.
-    :param loss: Loss function, defaults to Trace_ELBO.
-    :param lr: Learning rate, defaults to 0.01.
-    :param rng_key: Random seed, defaults to 254.
-    :param num_epochs: Number of epochs to train the model, defaults to 5000.
-    :param num_samples: Number of posterior samples.
-    :param log_func: Logging function, defaults to print.
-    :param log_freq: Frequency of logging, defaults to 0 (no logging).
-    :param to_numpy: Convert the posterior distribution to numpy array(s),
-        defaults to True.
+
+    Parameters
+    ----------
+    model :
+        A pyro model.
+    guide :
+        A pyro guide.
+    loss :
+        Loss function, defaults to Trace_ELBO.
+    optimizer :
+        Optimizer function, defaults to torch.optim.Adam.
+    scheduler :
+        Scheduler function, defaults to pyro.optim.ReduceLROnPlateau.
+    seed :
+        Random seed, defaults to None.
+    num_epochs :
+        Number of epochs to train the model, defaults to 30000.
+    log_freq :
+        Frequency of logging, defaults to 10.
+    checkpoint_freq :
+        Frequency of checkpoints, defaults to -1.
+    to_numpy :
+        Convert the posterior distribution to numpy array(s), defaults to True.
+    optimizer_kwargs :
+        Additional keyword arguments for the optimizer, defaults to {"lr": 1e-2}.
+    scheduler_kwargs :
+        Additional keyword arguments for the scheduler, defaults to {"factor": 0.99}.
+    loss_kwargs :
+        Additional keyword arguments for the loss function, defaults to {"num_particles": 1}.
+
+    Attributes
+    ----------
+    learning_rates :
+        Tracks the learning rates during training.
+    gradient_norms :
+        Tracks the gradient norms during training.
+    checkpoints :
+        Stores checkpoints during training.
     """
 
     def __init__(
@@ -77,22 +104,54 @@ class SVIBaseHandler:
         self.steps = 0
 
     def _update_state(self, loss: Union[List[float], np.ndarray]) -> None:
+        """
+        Update the state of the handler with the given loss.
+
+        Parameters
+        ----------
+        loss :
+            Loss values to update the state with.
+        """
         if isinstance(loss, list):
             loss = np.asarray(loss)
         self.loss = loss if self.loss is None else np.concatenate([self.loss, loss])
 
     def _to_numpy(self, posterior: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+        """
+        Convert the posterior distribution to a numpy array.
+
+        Parameters
+        ----------
+        posterior :
+            Posterior distribution to be converted.
+
+        Returns
+        -------
+            Converted posterior distribution.
+        """
+
         return {k: v.detach().cpu().numpy() for k, v in posterior.items()}
 
     def _init_svi(self) -> SVI:
         """
-        Initialises the SVI.
+        Initialize the SVI.
+
+        Returns
+        -------
+            Initialized SVI object.
         """
         return SVI(self.model, self.guide, self.optimizer, loss=self.loss)
 
     def _set_lr(self, lr: float) -> None:
-        # this works for CLIPPED adam
-        # checl if optim_objs is presetn
+        """
+        Set the learning rate for the optimizer.
+
+        Parameters
+        ----------
+        lr : float
+            Learning rate to be set.
+        """
+
         if hasattr(self.optimizer, "optim_objs"):
             for opt in self.optimizer.optim_objs.values():
                 for group in opt.param_groups:
@@ -104,8 +163,12 @@ class SVIBaseHandler:
 
     def _get_learning_rate(self) -> Any:
         """
-        Extracts the learning rate from the first parameter.
-        TODO: return a dict(lr: [params])
+        Extract the learning rate from the first parameter.
+
+        Returns
+        -------
+        float
+            Current learning rate.
         """
         for name, param in self.optimizer.get_state().items():
             if "optimizer" in param:
@@ -117,7 +180,7 @@ class SVIBaseHandler:
 
     def _track_learning_rate(self) -> None:
         """
-        Tracks the learning rate during training.
+        Track the learning rate during training.
         """
         for name, param in self.optimizer.get_state().items():
             if "optimizer" in param:
@@ -126,12 +189,28 @@ class SVIBaseHandler:
                 self.learning_rates[name].append(param["param_groups"][0]["lr"])
 
     def _track_gradient_norms(self) -> None:
-        # Register hooks to monitor gradient norms.
+        """
+        Register hooks to monitor gradient norms.
+        """
 
         for name, value in pyro.get_param_store().named_parameters():
             value.register_hook(lambda g, name=name: self.gradient_norms[name].append(g.norm().item()))
 
     def _fit(self, *args: torch.Tensor, **kwargs: torch.Tensor) -> np.ndarray:
+        """
+        Fit the model with the given arguments.
+
+        Parameters
+        ----------
+        *args :
+            Positional arguments for the model.
+        **kwargs :
+            Keyword arguments for the model.
+
+        Returns
+        -------
+            Array of loss values during training.
+        """
         losses = []
         pbar = tqdm(range(self.steps, self.steps + self.num_epochs))
         failure = False
@@ -192,6 +271,20 @@ class SVIBaseHandler:
     def fit(
         self, num_epochs: Optional[int] = None, lr: Optional[float] = None, *args: torch.Tensor, **kwargs: torch.Tensor
     ) -> None:
+        """
+        Train the model with the given number of epochs and learning rate.
+
+        Parameters
+        ----------
+        num_epochs :
+            Number of epochs to train the model.
+        lr :
+            Learning rate for training.
+        *args :
+            Positional arguments for the model.
+        **kwargs :
+            Keyword arguments for the model.
+        """
         if num_epochs is not None:
             self.num_epochs = num_epochs
 
@@ -205,6 +298,24 @@ class SVIBaseHandler:
     def predict(
         self, return_sites: List[str], num_samples: int = 25, *args: Union[str, List[str]], **kwargs: Any
     ) -> None:
+        """
+        Predict using the trained model.
+
+        Parameters
+        ----------
+        return_sites :
+            List of sites to return from the prediction.
+        num_samples :
+            Number of posterior samples, defaults to 25.
+        *args :
+            Positional arguments for the model.
+        **kwargs :
+            Keyword arguments for the model.
+
+        Returns
+        -------
+        None
+        """
         if self.params is not None:
             pyro.clear_param_store()
             pyro.get_param_store().set_state(self.params)
