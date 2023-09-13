b78f0ae4c08e84f7eec5006fc9a1eafaa450523c
Harald Vohringer <harald.voeh@gmail.com>
2023-09-13 16:37:32 +0200

    added get_ordered_genes function

--- scpca/utils/data.py
+++ scpca/utils/data.py
@@ -1,11 +1,14 @@
 from typing import Any, List, Optional, Tuple, Union
 
 import numpy as np
+import pandas as pd  # type: ignore
 from anndata import AnnData  # type: ignore
 from numpy.typing import NDArray
 from scipy.sparse import issparse  # type: ignore
 
 from ..logger import logger
+
+DESIGN_KEY = "design"
 
 
 def _get_rna_counts(adata: AnnData, layers_key: Optional[str] = None) -> NDArray[np.float32]:
@@ -57,9 +60,6 @@ def _get_rna_counts(adata: AnnData, layers_key: Optional[str] = None) -> NDArray
     return np.asarray(X).astype(np.float32)
 
 
-DESIGN_KEY = "design"
-
-
 def _get_model_design(adata: AnnData, model_key: str, reverse: bool = False) -> Any:
     """
     Extracts the design dictionary from an AnnData object.
@@ -69,16 +69,15 @@ def _get_model_design(adata: AnnData, model_key: str, reverse: bool = False) -> 
 
     Parameters
     ----------
-    adata : AnnData
+    adata :
         The AnnData object containing the model annotations.
-    model_key : str
+    model_key :
         The key identifying the model in the `uns` attribute.
-    reverse : bool
+    reverse :
         Whether to reverse the key/items in the returned dict.
 
     Returns
     -------
-    dict
         The design dictionary associated with the specified model.
 
     Raises
@@ -126,7 +125,6 @@ def _validate_sign(sign: Union[float, int]) -> Union[float, int]:
 
     Returns
     -------
-    Union[float, int]
         The validated sign.
 
     Raises
@@ -158,7 +156,6 @@ def _validate_states(states: Union[List[str], Tuple[str, str], str]) -> Tuple[st
 
     Returns
     -------
-    tuple
         A tuple containing two state names and their corresponding indices.
 
     Raises
@@ -181,3 +178,105 @@ def _validate_states(states: Union[List[str], Tuple[str, str], str]) -> Tuple[st
         raise TypeError("The 'states' parameter must be either a list or a string.")
 
     return state_a, state_b
+
+
+def _get_gene_idx(array: NDArray[np.float32], highest: int, lowest: int) -> NDArray[np.int64]:
+    """
+    Given an array of indices return the highest and/or lowest
+    indices.
+
+    Parameters
+    ----------
+    array: np.ndarray
+        array in which to extract the highest/lowest indices
+    highest: int
+        number of top indices to extract
+    lowest: int
+        number of lowest indices to extract
+
+    Returns
+    -------
+    np.ndarray
+    """
+    order = np.argsort(array)
+
+    if highest == 0:
+        gene_idx = order[:lowest]
+    else:
+        gene_idx = np.concatenate([order[:lowest], order[-highest:]])
+
+    return gene_idx
+
+
+def get_ordered_genes(
+    adata: AnnData,
+    model_key: str,
+    state: str,
+    factor: int,
+    sign: Union[int, float] = 1.0,
+    vector: str = "W_rna",
+    highest: int = 10,
+    lowest: int = 0,
+    ascending: bool = False,
+) -> pd.DataFrame:
+    """
+    Retrieve the ordered genes based on differential factor values.
+
+    Parameters
+    ----------
+    adata :
+        Annotated data object containing gene expression data.
+    model_key :
+        Key to identify the specific model.
+    state :
+        Name of the model state from which to extract genes.
+    factor :
+        Factor index for which differential factor values are calculated.
+    sign :
+        Sign multiplier for differential factor values. Default is 1.0.
+    vector :
+        Vector type from which to extract differential factor values. Default is "W_rna".
+    highest :
+        Number of genes with the highest differential factor values to retrieve. Default is 10.
+    lowest :
+        Number of genes with the lowest differential factor values to retrieve. Default is 0.
+    ascending :
+        Flag indicating whether to sort genes in ascending order based on differential factor values.
+        Default is False.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the ordered genes along with their magnitude, differential factor values,
+        type (lowest/highest), model state, factor index, and gene index.
+
+    Raises
+    ------
+    ValueError
+        If the specified model key or model state is not found in the provided AnnData object.
+    """
+    _ = _validate_sign(sign)
+    model_design = _get_model_design(adata, model_key)
+    state = model_design[state]
+    weights = sign * adata.varm[f"{model_key}_{vector}"][..., factor, state]
+    gene_idx = _get_gene_idx(weights, highest, lowest)
+
+    magnitude = np.abs(weights[gene_idx])
+    genes = adata.var_names.to_numpy()[gene_idx]
+
+    return (
+        pd.DataFrame(
+            {
+                "gene": genes,
+                "magnitude": magnitude,
+                "diff": weights[gene_idx],
+                "type": ["lowest"] * lowest + ["highest"] * highest,
+                "state": state,
+                "factor": factor,
+                "index": gene_idx,
+            }
+        )
+        .sort_values(by="diff", ascending=ascending)
+        .reset_index(drop=True)
+        .rename(columns={"diff": "value"})
+    )
