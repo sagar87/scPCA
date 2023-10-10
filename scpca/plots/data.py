9f84534a09c4cfaa0eb23214435086eff8bf5ae9
Harald Vohringer <harald.voeh@gmail.com>
2023-10-10 13:36:29 +0200

    add design_matrix plotting function

--- scpca/plots/data.py
+++ scpca/plots/data.py
@@ -3,12 +3,15 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 import matplotlib.collections as collections  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
+from anndata import AnnData  # type: ignore
 from matplotlib.colors import (  # type: ignore
     LinearSegmentedColormap,
     hsv_to_rgb,
     rgb_to_hsv,
 )
 from numpy.typing import NDArray
+from patsy import dmatrix  # type: ignore
+from patsy.design_info import DesignMatrix  # type: ignore
 
 
 def _get_hsvcmap(i: int, N: int, rot: float = 0.0) -> LinearSegmentedColormap:
@@ -192,3 +195,78 @@ def data_matrix(
         if ylabel_pos is not None:
             ax.yaxis.set_label_coords(ylabel_pos[0], ylabel_pos[1])
     return ax
+
+
+def get_design_matrix(dm: DesignMatrix, repeats: int = 4, cat_repeats: Optional[int] = None) -> NDArray[np.float32]:
+    if isinstance(dm, DesignMatrix):
+        m = np.asmatrix(dm)
+    else:
+        m = dm
+
+    _, idx = np.unique(m, return_index=True, axis=0)
+    mu = m[np.sort(idx)]
+    mr = np.repeat(mu, repeats=repeats, axis=0)
+    if cat_repeats is not None:
+        mr = np.concatenate([mr] * cat_repeats, axis=0)
+
+    return mr
+
+
+def design_matrix(
+    adata: AnnData,
+    formula: str,
+    repeats: int = 4,
+    cat_repeats: Optional[int] = None,
+    xticklabels: List[str] = [],
+    title: str = "D",
+    xlabel: Optional[str] = None,
+    ylabel: Optional[str] = None,
+    ylabel_pos: Optional[Tuple[float, float]] = None,
+    xlabel_pos: Optional[Tuple[float, float]] = None,
+    rotation: int = 90,
+    col: Optional[int] = None,
+    ax: plt.Axes = None,
+) -> plt.Axes:
+    if ax is None:
+        plt.figure(figsize=(0.8, 3))
+        ax = plt.gca()
+
+    design_matrix = dmatrix(formula, adata.obs)
+    M = get_design_matrix(design_matrix, repeats=repeats, cat_repeats=cat_repeats)
+    if col is None:
+        g = ax.imshow(M, cmap="Greys", vmin=0, vmax=1)
+    else:
+        M = M[:, [col]]
+        g = ax.imshow(M, cmap="Greys", vmin=0, vmax=1)
+    _ = g.axes.set_xticks([i + 0.5 for i in range(M.shape[1])])
+
+    _ = g.axes.set_yticks([])
+    if title is not None:
+        g.axes.set_title(f"{title}")
+
+    ax.spines["top"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+
+    xgrid = np.arange(M.shape[0] + 1) - 0.5
+    ygrid = np.arange(M.shape[1] + 1) - 0.5
+    ax.hlines(xgrid, ygrid[0], ygrid[-1], color="k")
+    ax.vlines(ygrid, xgrid[0], xgrid[-1], color="k")
+
+    if len(xticklabels) == 0:
+        _ = g.axes.set_xticks([])
+    else:
+        _ = g.axes.set_xticks([i for i in range(M.shape[1])])
+        _ = g.axes.set_xticklabels(xticklabels, rotation=rotation)
+
+    if xlabel is not None:
+        ax.set_xlabel("$%s$" % xlabel)
+        if xlabel_pos is not None:
+            ax.xaxis.set_label_coords(xlabel_pos[0], xlabel_pos[1])
+    if ylabel is not None:
+        ax.set_ylabel("$%s$" % ylabel)
+        if ylabel_pos is not None:
+            ax.yaxis.set_label_coords(ylabel_pos[0], ylabel_pos[1])
+
+    return g
