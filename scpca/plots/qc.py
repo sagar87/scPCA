b94d19573675ebcafb0d33c721fa9cddd7eeea39
Harald Vohringer <harald.voeh@gmail.com>
2023-10-10 10:09:00 +0200

    Added docstrings to qc functions.

--- scpca/plots/qc.py
+++ scpca/plots/qc.py
@@ -13,6 +13,11 @@ from sklearn.metrics import mean_squared_error  # type: ignore
 from ..utils.data import _get_rna_counts
 
 
+def _var(concentration: Union[float, NDArray[np.float32]], mean: Union[float, NDArray[np.float32]]) -> Any:
+    """Computes the expected variance of the Gamma Poission distribution."""
+    return concentration / (concentration / mean) ** 2 * (1 + concentration / mean)
+
+
 def true_pred(
     adata: AnnData,
     model_key: str,
@@ -24,28 +29,35 @@ def true_pred(
     ax: plt.Axes = None,
 ) -> plt.Axes:
     """
-    Plots a 2D histogram of the predicted counts against the true counts.
+    Create a 2D histogram plot of predicted vs. true counts of a scPCA model.
 
     Parameters
     ----------
-    adata: AnnData
-        AnnData object.
-    model_key: str, optional (default: "scpca")
-        Key for the fitted model.
-    layers_key: str, optional (default: None)
-        If `layers_key` is None, then the raw counts are extracted from `adata.X`.
-        Otherwise, the counts are extracted from `adata.layers[layers_key]`.
-    protein_obsm_key: str, optional (default: None)
-        Key for protein counts in `adata.obsm`. Providing `protein_obsm_key`
-        overrides `layers_key`, i.e. protein counts are plotted.
-    cmap: str, optional (default: "viridis")
-        Colormap for the scatterplot. Color represents the mean of the counts.
-    ax: matplotlib.axes.Axes, optional (default: None)
-        Axes to plot on. If None, then a new figure is created.
+    adata :
+        Anndata object containing single-cell RNA-seq data.
+    model_key :
+        Key for the fitted model within the AnnData object.
+    layers_key :
+        Key to extract counts from adata.layers. If None, raw counts are extracted from adata.X.
+    cmap :
+        Colormap for the scatterplot. Default is "viridis".
+    colorbar_pos :
+        Position of the colorbar (e.g., "right" or "left"). Default is "right".
+    colorbar_width :
+        Width of the colorbar as a percentage of the figure. Default is "3%".
+    orientation :
+        Orientation of the colorbar ("vertical" or "horizontal"). Default is "vertical".
+    ax :
+        Existing matplotlib axes to plot on. If None, a new figure is created.
 
     Returns
     -------
-    ax: matplotlib.axes.Axes
+        Matplotlib axes containing the 2D scatter plot.
+
+    Notes
+    -----
+    This function generates a 2D-histogram plot comparing predicted counts (from a fitted model)
+    against true counts in single-cell RNA-seq data.
     """
     if ax is None:
         fig = plt.figure()
@@ -88,11 +100,6 @@ def true_pred(
     return ax
 
 
-def vart(concentration: Union[float, NDArray[np.float32]], mean: Union[float, NDArray[np.float32]]) -> Any:
-    """Computes the expected variance of the Gamma Poission distribution."""
-    return concentration / (concentration / mean) ** 2 * (1 + concentration / mean)
-
-
 def mean_var(
     adata: AnnData,
     model_key: Union[str, None] = None,
@@ -105,6 +112,43 @@ def mean_var(
     cmap: str = "viridis",
     ax: plt.Axes = None,
 ) -> plt.Axes:
+    """
+    Create a Scatter Plot of Mean vs. Variance in Single-Cell RNA-seq Data.
+
+    Parameters
+    ----------
+    adata :
+        Anndata object containing single-cell RNA-seq data.
+    model_key :
+        Key for the fitted model within the AnnData object. (Default: None)
+    layers_key :
+        Key to extract counts from adata.layers. If None, raw counts are extracted from adata.X.
+    highest :
+        Number of highest-deviating genes to label on the plot. (Default: None)
+    β_rna_mean :
+        Prior mean for RNA expression. (Default: 3.0)
+    β_rna_sd :
+        Prior standard deviation for RNA expression. (Default: 1.0)
+    alpha :
+        Transparency of data points in the scatter plot. (Default: 1.0)
+    margin :
+        Margin for the variance plot. (Default: 0.01)
+    cmap :
+        Colormap for the scatterplot. (Default: "viridis")
+    ax :
+        Existing matplotlib axes to plot on. If None, a new figure is created.
+
+    Returns
+    -------
+        Matplotlib axes containing the scatter plot.
+
+    Notes
+    -----
+    This function creates a scatter plot of the mean expression values against the variance
+    for genes in single-cell RNA-seq data. It also visualizes a prior distribution for variance.
+    The plot may include labeled genes with the highest deviations from the expected variance.
+    """
+
     if ax is None:
         plt.figure()
         ax = plt.gca()
@@ -126,13 +170,13 @@ def mean_var(
 
     true_mean = np.mean(counts, axis=0)
     true_var = np.var(counts, axis=0)
-    theoretical = vart(β_rna_mean, np.logspace(-4, 3, 1000))
-    expectation = vart(β_rna_mean, true_mean)
+    theoretical = _var(β_rna_mean, np.logspace(-4, 3, 1000))
+    expectation = _var(β_rna_mean, true_mean)
 
     ax.fill_between(
         np.logspace(-4, 3, 1000),
-        vart(lower, np.logspace(-4, 3, 1000)),
-        vart(upper, np.logspace(-4, 3, 1000)),
+        _var(lower, np.logspace(-4, 3, 1000)),
+        _var(upper, np.logspace(-4, 3, 1000)),
         color="C3",
         alpha=0.2,
     )
@@ -141,7 +185,7 @@ def mean_var(
         true_var,
         alpha=alpha,
         s=10,
-        c=adata.varm[f"α_{model_key}"] if model_key is not None else None,
+        c=1.0 / adata.varm[f"α_{model_key}"] if model_key is not None else None,
         cmap="viridis",
         norm=LogNorm(),
     )
@@ -154,8 +198,6 @@ def mean_var(
         linestyle="--",
         label=f"Prior mean {β_rna_mean:.2f}",
     )
-    # ax.plot(np.logspace(-3, 3, 1000), vart(upper, np.logspace(-3, 3, 1000)), color='C3', linestyle='--')
-    # ax.plot(np.logspace(-3, 3, 1000), vart(lower, np.logspace(-3, 3, 1000)), color='C3', linestyle='--')
 
     ax.legend()
     ax.set_yscale("log")
@@ -169,31 +211,9 @@ def mean_var(
     if highest is not None:
         deviation = np.abs((true_var - expectation) / expectation)
         highest_genes = np.argsort(deviation)[-highest:]
-        genes = adata.var_names[highest_genes]
+        # genes = adata.var_names[highest_genes]
 
         texts = [ax.text(true_mean[h], true_var[h], adata.var_names[h], fontsize=10) for h in highest_genes]
         adjust_text(texts, arrowprops=dict(arrowstyle="-", color="k", lw=0.5))
-
-        print(true_mean[highest_genes])
-        # ta.allocate_text(
-        #     fig,
-        #     ax,
-        #     true_mean[highest_genes],
-        #     true_var[highest_genes],
-        #     genes,
-        #     x_scatter=true_mean[highest_genes],
-        #     y_scatter=true_var[highest_genes],
-        #     textsize=10,
-        #     linecolor="grey",
-        #     min_distance=repel,
-        #     max_distance=max_distance,
-        #     margin=margin,
-        # )
-        # txt_height = np.log(0.04*(ax.get_ylim()[1] - ax.get_ylim()[0]))
-        # txt_width = np.log(0.02*(ax.get_xlim()[1] - ax.get_xlim()[0]))
-        # text_positions = get_text_positions(true_mean[highest_genes], true_var[highest_genes], txt_width, txt_height)
-        # text_plotter(true_mean[highest_genes], true_var[highest_genes], text_positions, ax, txt_width, txt_height)
-
-        print(genes)
 
     return ax
