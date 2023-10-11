30800de97e1bfe9a5264301e3b6bde1ad23e94db
Harald Vohringer <harald.voeh@gmail.com>
2023-10-11 15:53:53 +0200

    export factor strip

--- scpca/plots/__init__.py
+++ scpca/plots/__init__.py
@@ -1,5 +1,5 @@
 from .data import data_matrix, design_matrix, triangle_overlay
-from .factor_embedding import factor_density, factor_embedding
+from .factor_embedding import factor_density, factor_embedding, factor_strip
 from .loadings_scatter import loadings_scatter
 from .loadings_state import loadings_state
 from .qc import mean_var, true_pred
@@ -7,6 +7,7 @@ from .qc import mean_var, true_pred
 __all__ = [
     "factor_embedding",
     "factor_density",
+    "factor_strip",
     "loadings_state",
     "loadings_scatter",
     "true_pred",
