b7f4d0a4d52389e0fb798d7d281c977cdccb4030
Harald Vohringer <harald.voeh@gmail.com>
2025-02-28 11:04:40 +0100

    fixes in format.sh

--- format.sh
+++ format.sh
@@ -1,5 +1,6 @@
 poetry run black .
 poetry run isort . --profile black
-poetry run flake8 .
+poetry run flake8 scpca
+poetry run flake8 tests
 poetry run coverage run -m --source=scpca pytest tests
 poetry run coverage report
